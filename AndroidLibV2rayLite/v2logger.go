3ae76fab72c536d26767efa75fafce8afecb68e1
2dust <31833384+2dust@users.noreply.github.com>
2019-08-18 18:15:37 +0800

    add lib

---
+++ AndroidLibV2rayLite/v2logger.go
@@ -0,0 +1,32 @@
+package libv2ray
+
+// This struct creates our own log writer without datatime stamp
+// As Android adds time stamps on each line
+
+import (
+	"log"
+	"os"
+
+	v2commlog "v2ray.com/core/common/log"
+)
+
+type consoleLogWriter struct {
+	logger *log.Logger
+}
+
+func (w *consoleLogWriter) Write(s string) error {
+	w.logger.Print(s)
+	return nil
+}
+
+func (w *consoleLogWriter) Close() error {
+	return nil
+}
+
+// This logger won't print data/time stamps
+func createStdoutLogWriter() v2commlog.WriterCreator {
+	return func() v2commlog.Writer {
+		return &consoleLogWriter{
+			logger: log.New(os.Stdout, "", 0)}
+	}
+}
