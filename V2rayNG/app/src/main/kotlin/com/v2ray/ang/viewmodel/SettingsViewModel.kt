c9a6a459d4b555166a88cd8b03fe13fbaefb673b
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2020-12-05 23:41:07 -0500

    Fix update full config when settings change
    Now daemon process does not reference the node list at all and
    only depend on a couple of settings like PREF_CURR_CONFIG..

---
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,46 @@
+package com.v2ray.ang.viewmodel
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.content.SharedPreferences
+import android.support.v7.preference.PreferenceManager
+import android.util.Log
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.ui.SettingsActivity.Companion
+import com.v2ray.ang.util.AngConfigManager
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class SettingsViewModel(application: Application) : AndroidViewModel(application), SharedPreferences.OnSharedPreferenceChangeListener {
+    fun startListenPreferenceChange() {
+        PreferenceManager.getDefaultSharedPreferences(getApplication()).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onCleared() {
+        PreferenceManager.getDefaultSharedPreferences(getApplication()).unregisterOnSharedPreferenceChangeListener(this)
+        Log.i(AppConfig.ANG_PACKAGE, "Settings ViewModel is cleared")
+        super.onCleared()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
+        Log.d(AppConfig.ANG_PACKAGE, "Observe settings changed: $key")
+        when(key) {
+            Companion.PREF_SNIFFING_ENABLED,
+            Companion.PREF_PROXY_SHARING,
+            Companion.PREF_LOCAL_DNS_ENABLED,
+            Companion.PREF_REMOTE_DNS,
+            Companion.PREF_DOMESTIC_DNS,
+            Companion.PREF_ROUTING_DOMAIN_STRATEGY,
+            Companion.PREF_ROUTING_MODE,
+            AppConfig.PREF_V2RAY_ROUTING_AGENT,
+            AppConfig.PREF_V2RAY_ROUTING_BLOCKED,
+            AppConfig.PREF_V2RAY_ROUTING_DIRECT -> {
+                GlobalScope.launch {
+                    if (!AngConfigManager.genStoreV2rayConfig(AngConfigManager.configs.index)) {
+                        Log.d(AppConfig.ANG_PACKAGE, "$key changed but generate full configuration failed!")
+                    }
+                }
+            }
+        }
+    }
+}
