4e7849d1cbf71898ed171e29fc8629cd32f0c9d9
yuhan6665 <1588741+yuhan6665@users.noreply.github.com>
2022-12-04 18:15:17 -0500

    Add Wireguard outbound (custom config)

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/EConfigType.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/EConfigType.kt
@@ -6,7 +6,8 @@ enum class EConfigType(val value: Int, val protocolScheme: String) {
     SHADOWSOCKS(3, "ss://"),
     SOCKS(4, "socks://"),
     VLESS(5, "vless://"),
-    TROJAN(6, "trojan://");
+    TROJAN(6, "trojan://"),
+    WIREGUARD(7, "wireguard://");
 
     companion object {
         fun fromInt(value: Int) = values().firstOrNull { it.value == value }
