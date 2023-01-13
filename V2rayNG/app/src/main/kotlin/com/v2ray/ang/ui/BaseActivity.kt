2990db305ea885d11555f065ce3ccaac803c1830
Sylvester Yao <admin@ysy950803.cn>
2023-01-13 12:00:59 +0800

    Replace colors hard code with values-night.

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/BaseActivity.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/BaseActivity.kt
@@ -2,12 +2,10 @@ package com.v2ray.ang.ui
 
 import android.content.Context
 import android.os.Build
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.v2ray.ang.util.MyContextWrapper
-import com.v2ray.ang.R
 import com.v2ray.ang.util.Utils
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -19,27 +17,6 @@ abstract class BaseActivity : AppCompatActivity() {
         else -> super.onOptionsItemSelected(item)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        checkDarkMode()
-    }
-
-    private fun checkDarkMode() {
-        if (Utils.getDarkModeStatus(this)) {
-            if (this.javaClass.simpleName == "MainActivity") {
-                setTheme(R.style.AppThemeDark_NoActionBar)
-            } else {
-                setTheme(R.style.AppThemeDark)
-            }
-        } else {
-            if (this.javaClass.simpleName == "MainActivity") {
-                setTheme(R.style.AppThemeLight_NoActionBar)
-            } else {
-                setTheme(R.style.AppThemeLight)
-            }
-        }
-    }
-
     @RequiresApi(Build.VERSION_CODES.N)
     override fun attachBaseContext(newBase: Context?) {
         val context = newBase?.let {
@@ -47,7 +24,4 @@ abstract class BaseActivity : AppCompatActivity() {
         }
         super.attachBaseContext(context)
     }
-
-
-
 }
