613e9ac8bf33ad42fb67f29d346c3b9eeab3561f
2dust <31833384+2dust@users.noreply.github.com>
2023-03-31 13:48:40 +0800

    Add permission  POST_NOTIFICATIONS and  READ_MEDIA_IMAGES

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import android.text.TextUtils
 import android.view.KeyEvent
 import com.v2ray.ang.AppConfig
 import android.content.res.ColorStateList
+import android.os.Build
 import com.google.android.material.navigation.NavigationView
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
@@ -107,6 +108,15 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         setupViewModel()
         copyAssets()
         migrateLegacy()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            RxPermissions(this)
+                .request(Manifest.permission.POST_NOTIFICATIONS)
+                .subscribe {
+                    if (!it)
+                        toast(R.string.toast_permission_denied)
+                }
+        }
     }
 
     private fun setupViewModel() {
