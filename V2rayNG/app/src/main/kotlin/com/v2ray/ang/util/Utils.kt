643a1fa2ff4c9eff19fdb637b59cd28ade1fdca0
Hiddify1 <Hiddify@gmail.com>
2023-05-01 17:48:00 +0200

    1.01.5

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/Utils.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/util/Utils.kt
@@ -384,7 +384,7 @@ object Utils {
 
     fun getLocale(context: Context): Locale =
         when (settingsStorage?.decodeString(AppConfig.PREF_LANGUAGE) ?: "auto") {
-            "auto" -> getSysLocale()
+            "auto" -> Locale("fa") //getSysLocale()
             "en" -> Locale("en")
             "zh-rCN" -> Locale("zh", "CN")
             "zh-rTW" -> Locale("zh", "TW")
