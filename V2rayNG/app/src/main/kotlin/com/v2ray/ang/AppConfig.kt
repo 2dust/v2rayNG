c8d9d7ca8009d2b886af8eccd059f5fcfdf7b26c
a-pav <14939129+a-pav@users.noreply.github.com>
2024-03-11 13:34:39 +0330

    Update AppConfig.kt

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
@@ -40,6 +40,7 @@ object AppConfig {
     const val PREF_BYPASS_APPS = "pref_bypass_apps"
     const val PREF_CONFIRM_REMOVE = "pref_confirm_remove"
     const val PREF_START_SCAN_IMMEDIATE = "pref_start_scan_immediate"
+    const val PREF_DARK_MODE_ENABLED = "pref_dark_mode_enabled"
     const val PREF_MUX_ENABLED = "pref_mux_enabled"
     const val PREF_MUX_CONCURRENCY = "pref_mux_concurency"
     const val PREF_MUX_XUDP_CONCURRENCY = "pref_mux_xudp_concurency"
