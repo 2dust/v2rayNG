3417b3c2aee961ab4be0f91323e9a4d8522ec5f6
Vahid Farid <vahidfarid@gmail.com>
2024-05-22 19:11:11 +0330

    Update V2RayTestService.kt

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayTestService.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayTestService.kt
@@ -6,6 +6,9 @@ import android.os.IBinder
 import com.v2ray.ang.AppConfig.MSG_MEASURE_CONFIG
 import com.v2ray.ang.AppConfig.MSG_MEASURE_CONFIG_CANCEL
 import com.v2ray.ang.AppConfig.MSG_MEASURE_CONFIG_SUCCESS
+import com.v2ray.ang.AppConfig.MSG_MEASURE_FRAGMENT
+import com.v2ray.ang.AppConfig.MSG_MEASURE_FRAGMENT_CANCEL
+import com.v2ray.ang.AppConfig.MSG_MEASURE_FRAGMENT_SUCCESS
 import com.v2ray.ang.util.MessageUtil
 import com.v2ray.ang.util.SpeedtestUtil
 import com.v2ray.ang.util.Utils
@@ -32,7 +35,17 @@ class V2RayTestService : Service() {
                     MessageUtil.sendMsg2UI(this@V2RayTestService, MSG_MEASURE_CONFIG_SUCCESS, Pair(contentPair.first, result))
                 }
             }
+            MSG_MEASURE_FRAGMENT -> {
+                val contentPair = intent.getSerializableExtra("content") as Pair<Int, String>
+                realTestScope.launch {
+                    val result = SpeedtestUtil.realPing(contentPair.second)
+                    MessageUtil.sendMsg2UI(this@V2RayTestService, MSG_MEASURE_FRAGMENT_SUCCESS, Pair(contentPair.first, result))
+                }
+            }
             MSG_MEASURE_CONFIG_CANCEL -> {
+                realTestScope.coroutineContext[Job]?.cancelChildren()
+            }
+            MSG_MEASURE_FRAGMENT_CANCEL -> {
                 realTestScope.coroutineContext[Job]?.cancelChildren()
             }
         }
