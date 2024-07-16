447e712a9dd1e46327a804bc94b4f0bf1ac3b1d2
2dust <31833384+2dust@users.noreply.github.com>
2024-07-16 14:05:34 +0800

    up 1.8.31

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 21
         targetSdk = 34
-        versionCode = 574
-        versionName = "1.8.30"
+        versionCode = 575
+        versionName = "1.8.31"
         multiDexEnabled = true
         splits.abi {
             reset()
