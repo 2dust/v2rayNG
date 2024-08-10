4299724ab8f7d030e3db0bea0aacfd6d73cc4299
Tamim Hossain <tamimh.dev@gmail.com>
2024-08-10 12:41:51 +0600

    Refactor EConfigType to use constants from AppConfig
    Updated the EConfigType enum to use protocol scheme constants from AppConfig. This change improves maintainability by centralizing protocol scheme definitions in a single location.

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/EConfigType.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/dto/EConfigType.kt
@@ -1,13 +1,16 @@
 package com.v2ray.ang.dto
 
+import com.v2ray.ang.AppConfig
+
+
 enum class EConfigType(val value: Int, val protocolScheme: String) {
-    VMESS(1, "vmess://"),
-    CUSTOM(2, ""),
-    SHADOWSOCKS(3, "ss://"),
-    SOCKS(4, "socks://"),
-    VLESS(5, "vless://"),
-    TROJAN(6, "trojan://"),
-    WIREGUARD(7, "wireguard://");
+    VMESS(1, AppConfig.VMESS),
+    CUSTOM(2, AppConfig.CUSTOM),
+    SHADOWSOCKS(3,AppConfig.SHADOWSOCKS),
+    SOCKS(4, AppConfig.SOCKS),
+    VLESS(5, AppConfig.VLESS),
+    TROJAN(6, AppConfig.TROJAN),
+    WIREGUARD(7, AppConfig.WIREGUARD);
 
     companion object {
         fun fromInt(value: Int) = values().firstOrNull { it.value == value }
