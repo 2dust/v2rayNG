c0fda6fcba8ba026338bee634642c8c63adb2a43
2dust <31833384+2dust@users.noreply.github.com>
2024-09-19 14:54:17 +0800

    up 1.9.1

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 21
         targetSdk = 34
-        versionCode = 591
-        versionName = "1.9.0"
+        versionCode = 592
+        versionName = "1.9.1"
         multiDexEnabled = true
         splits {
             abi {
