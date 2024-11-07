7a2d686c0ea7791c107e1c7ffb4427882893d96e
DecorativeFamily <185765765+decorativefamily@users.noreply.github.com>
2024-11-07 12:45:10 +0330

    Update ExampleUnitTest.java
    reformating coded

--- V2rayNG/app/src/test/java/com/v2ray/ang/ExampleUnitTest.java
+++ V2rayNG/app/src/test/java/com/v2ray/ang/ExampleUnitTest.java
@@ -1,13 +1,17 @@
 package com.v2ray.ang;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+    /**
+     * Test method to verify that the addition of 2 + 2 equals 4.
+     * @throws Exception if any exception occurs during the test.
+     */
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
