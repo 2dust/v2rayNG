a6c6feeb99af861acc6cd10839204d378523bf8b
DecorativeFamily <185765765+decorativefamily@users.noreply.github.com>
2024-11-07 12:22:56 +0330

    Update build.gradle.kts
    reformating coded

--- V2rayNG/build.gradle.kts
+++ V2rayNG/build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.android.library).apply(false)
+    alias(libs.plugins.android.kotlin).apply(false)
 }
