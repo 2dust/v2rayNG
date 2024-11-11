29e718e34ff9a16f309a3e87756d8239084ce146
TTG <ttgcanada@outlook.com>
2024-11-11 14:52:02 -0500

    Update DNS in AppConfig

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/AppConfig.kt
@@ -161,15 +161,20 @@ object AppConfig {
     const val GOOGLEAPIS_COM_DOMAIN = "googleapis.com"
 
     // Android Private DNS constants
-    const val DNS_PUB_DOMAIN = "dns.pub"
+    const val DNS_DNSPOD_DOMAIN = "dot.pub"
     const val DNS_ALIDNS_DOMAIN = "dns.alidns.com"
-    const val DNS_ONE_ONE_DOMAIN = "one.one.one.one"
+    const val DNS_CLOUDFLARE_DOMAIN = "one.one.one.one"
     const val DNS_GOOGLE_DOMAIN = "dns.google"
+    const val DNS_QUAD9_DOMAIN = "dns.quad9.net"
+    const val DNS_YANDEX_DOMAIN = "common.dot.dns.yandex.net"
 
-    val DNS_PUB_ADDRESSES = arrayListOf("1.12.12.12", "120.53.53.53")
+    
     val DNS_ALIDNS_ADDRESSES = arrayListOf("223.5.5.5", "223.6.6.6", "2400:3200::1", "2400:3200:baba::1")
-    val DNS_ONE_ONE_ADDRESSES = arrayListOf("1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001")
+    val DNS_CLOUDFLARE_ADDRESSES = arrayListOf("1.1.1.1", "1.0.0.1", "2606:4700:4700::1111", "2606:4700:4700::1001")
+    val DNS_DNSPOD_ADDRESSES = arrayListOf("1.12.12.12", "120.53.53.53")
     val DNS_GOOGLE_ADDRESSES = arrayListOf("8.8.8.8", "8.8.4.4", "2001:4860:4860::8888", "2001:4860:4860::8844")
+    val DNS_QUAD9_ADDRESSES = arrayListOf("9.9.9.9", "149.112.112.112", "2620:fe::fe", "2620:fe::9")
+    val DNS_YANDEX_ADDRESSES = arrayListOf("77.88.8.8", "77.88.8.1", "2a02:6b8::feed:0ff", "2a02:6b8:0:1::feed:0ff")
 
     const val DEFAULT_PORT = 443
     const val DEFAULT_SECURITY = "auto"
