85d4194328ae2a17da8aebdf815acd7fb7c3810b
DecorativeFamily <185765765+decorativefamily@users.noreply.github.com>
2024-11-12 06:10:11 +0330

    Update ItemTouchHelperAdapter.java

--- V2rayNG/app/src/main/java/com/v2ray/ang/helper/ItemTouchHelperAdapter.java
+++ V2rayNG/app/src/main/java/com/v2ray/ang/helper/ItemTouchHelperAdapter.java
@@ -20,39 +20,38 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 /**
- * Interface to listen for a move or dismissal event from a {@link ItemTouchHelper.Callback}.
+ * Interface for listening to move and dismissal events from a {@link ItemTouchHelper.Callback}.
+ * Implement this interface to handle item touch events in a RecyclerView.
  *
  * @author Paul Burke (ipaulpro)
  */
 public interface ItemTouchHelperAdapter {
 
     /**
-     * Called when an item has been dragged far enough to trigger a move. This is called every time
-     * an item is shifted, and <strong>not</strong> at the end of a "drop" event.<br/>
-     * <br/>
-     * Implementations should call {@link RecyclerView.Adapter#notifyItemMoved(int, int)} after
-     * adjusting the underlying data to reflect this move.
+     * Called when an item has been dragged far enough to trigger a move.
+     * This method is called every time an item is shifted, not just at the end of a "drop" event.
      *
-     * @param fromPosition The start position of the moved item.
-     * @param toPosition   Then resolved position of the moved item.
-     * @return True if the item was moved to the new adapter position.
-     * @see RecyclerView#getAdapterPositionFor(RecyclerView.ViewHolder)
-     * @see RecyclerView.ViewHolder#getAdapterPosition()
+     * Implementations should call {@link RecyclerView.Adapter#notifyItemMoved(int, int)} 
+     * after adjusting the underlying data to reflect this move.
+     *
+     * @param fromPosition The starting position of the moved item.
+     * @param toPosition   The resolved position of the moved item.
+     * @return True if the item was successfully moved to the new adapter position.
      */
     boolean onItemMove(int fromPosition, int toPosition);
 
-
+    /**
+     * Called when an item move operation is completed.
+     */
     void onItemMoveCompleted();
 
     /**
-     * Called when an item has been dismissed by a swipe.<br/>
-     * <br/>
-     * Implementations should call {@link RecyclerView.Adapter#notifyItemRemoved(int)} after
-     * adjusting the underlying data to reflect this removal.
+     * Called when an item has been dismissed by a swipe.
      *
-     * @param position The position of the item dismissed.
-     * @see RecyclerView#getAdapterPositionFor(RecyclerView.ViewHolder)
-     * @see RecyclerView.ViewHolder#getAdapterPosition()
+     * Implementations should call {@link RecyclerView.Adapter#notifyItemRemoved(int)} 
+     * after adjusting the underlying data to reflect this removal.
+     *
+     * @param position The position of the dismissed item.
      */
     void onItemDismiss(int position);
 }
