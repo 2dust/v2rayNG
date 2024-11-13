7bd470dde895a7104a3978143aaf4f860233f8ac
DecorativeFamily <185765765+decorativefamily@users.noreply.github.com>
2024-11-13 17:44:06 +0330

    Create ExampleUnitTest.kts
    Create ExampleUnitTest.kts

---
+++ V2rayNG/app/src/test/kotlin/com/v2ray/ang/ExampleUnitTest.kts
@@ -0,0 +1,15 @@
+package com.v2ray.ang
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+class ExampleUnitTest {
+    @Test
+    @Throws(Exception::class)
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
