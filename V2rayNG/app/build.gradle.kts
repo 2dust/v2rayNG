a45eefc19f68f48547251988f003d2e88ba20a96
phoenix6936 <189024923+phoenix6936@users.noreply.github.com>
2024-11-21 10:52:10 +0330

    Update build.gradle.kts
    isMinifyEnabled : If enabled, your code will be minified.

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
