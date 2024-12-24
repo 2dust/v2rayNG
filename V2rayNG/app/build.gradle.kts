774ab09a1415c9ce93c61f4651ae71ec612b50f0
alphax-hue3682 <191818854+alphax-hue3682@users.noreply.github.com>
2024-12-24 10:36:32 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -98,7 +98,7 @@ android {
                 }
         } else {
             val versionCodes =
-                mapOf("armeabi-v7a" to 4, "arm64-v8a" to 4, "x86" to 3 )
+                mapOf("armeabi-v7a" to 4, "arm64-v8a" to 4, "x86" to 3)
 
             variant.outputs
                 .map { it as com.android.build.gradle.internal.api.ApkVariantOutputImpl }
