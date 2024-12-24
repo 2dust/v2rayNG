ad7e4e631833ac5b7e47816f3290286ba36719cc
alphax-hue3682 <191818854+alphax-hue3682@users.noreply.github.com>
2024-12-24 10:40:20 +0330

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
