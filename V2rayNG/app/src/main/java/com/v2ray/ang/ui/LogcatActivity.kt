a4edf86195dcc11049d384d21b384e7841dbf1d6
2dust <31833384+2dust@users.noreply.github.com>
2025-01-13 12:50:35 +0800

    Improved logcat

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/LogcatActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/LogcatActivity.kt
@@ -1,13 +1,13 @@
 package com.v2ray.ang.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.text.method.ScrollingMovementMethod
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityLogcatBinding
@@ -19,9 +19,11 @@ import kotlinx.coroutines.withContext
 import java.io.IOException
 
 class LogcatActivity : BaseActivity() {
-    private val binding by lazy {
-        ActivityLogcatBinding.inflate(layoutInflater)
-    }
+    private val binding by lazy { ActivityLogcatBinding.inflate(layoutInflater) }
+
+    var logsetsAll: MutableList<String> = mutableListOf()
+    var logsets: MutableList<String> = mutableListOf()
+    private val adapter by lazy { LogcatRecyclerAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,24 +31,20 @@ class LogcatActivity : BaseActivity() {
 
         title = getString(R.string.title_logcat)
 
-        logcat(false)
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        getLogcat()
     }
 
-    private fun logcat(shouldFlushLog: Boolean) {
+    private fun getLogcat() {
 
         try {
             binding.pbWaiting.visibility = View.VISIBLE
 
             lifecycleScope.launch(Dispatchers.Default) {
-                if (shouldFlushLog) {
-                    val lst = LinkedHashSet<String>()
-                    lst.add("logcat")
-                    lst.add("-c")
-                    withContext(Dispatchers.IO) {
-                        val process = Runtime.getRuntime().exec(lst.toTypedArray())
-                        process.waitFor()
-                    }
-                }
                 val lst = LinkedHashSet<String>()
                 lst.add("logcat")
                 lst.add("-d")
@@ -57,15 +55,34 @@ class LogcatActivity : BaseActivity() {
                 val process = withContext(Dispatchers.IO) {
                     Runtime.getRuntime().exec(lst.toTypedArray())
                 }
-//                val bufferedReader = BufferedReader(
-//                        InputStreamReader(process.inputStream))
-//                val allText = bufferedReader.use(BufferedReader::readText)
-                val allText = process.inputStream.bufferedReader().use { it.readText() }
+
+                val allText = process.inputStream.bufferedReader().use { it.readLines() }
                 launch(Dispatchers.Main) {
-                    binding.tvLogcat.text = allText
-                    binding.tvLogcat.movementMethod = ScrollingMovementMethod()
+                    logsetsAll = allText.toMutableList()
+                    logsets = allText.toMutableList()
+                    adapter.notifyDataSetChanged()
                     binding.pbWaiting.visibility = View.GONE
-                    Handler(Looper.getMainLooper()).post { binding.svLogcat.fullScroll(View.FOCUS_DOWN) }
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun clearLogcat() {
+        try {
+            lifecycleScope.launch(Dispatchers.Default) {
+                val lst = LinkedHashSet<String>()
+                lst.add("logcat")
+                lst.add("-c")
+                withContext(Dispatchers.IO) {
+                    val process = Runtime.getRuntime().exec(lst.toTypedArray())
+                    process.waitFor()
+                }
+                launch(Dispatchers.Main) {
+                    logsetsAll.clear()
+                    logsets.clear()
+                    adapter.notifyDataSetChanged()
                 }
             }
         } catch (e: IOException) {
@@ -75,21 +92,51 @@ class LogcatActivity : BaseActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_logcat, menu)
+
+        val searchItem = menu.findItem(R.id.search_view)
+        if (searchItem != null) {
+            val searchView = searchItem.actionView as SearchView
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean = false
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    filterLogs(newText)
+                    return false
+                }
+            })
+            searchView.setOnCloseListener {
+                filterLogs("")
+                false
+            }
+        }
+
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.copy_all -> {
-            Utils.setClipboard(this, binding.tvLogcat.text.toString())
+            Utils.setClipboard(this, logsets.joinToString("\n"))
             toast(R.string.toast_success)
             true
         }
 
         R.id.clear_all -> {
-            logcat(true)
+            clearLogcat()
             true
         }
 
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun filterLogs(content: String?): Boolean {
+        val key = content?.trim()
+        logsets = if (key.isNullOrEmpty()) {
+            logsetsAll.toMutableList()
+        } else {
+            logsetsAll.filter { it.contains(key) }.toMutableList()
+        }
+
+        adapter?.notifyDataSetChanged()
+        return true
     }
 }
