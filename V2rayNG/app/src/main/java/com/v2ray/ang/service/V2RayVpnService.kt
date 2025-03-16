159a3702867e085a0bf035cc196219db0551f0f6
2dust <31833384+2dust@users.noreply.github.com>
2025-03-16 14:44:25 +0800

    Changed tun2socks config private vlan
    https://github.com/2dust/v2rayNG/issues/4373

--- V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayVpnService.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/service/V2RayVpnService.kt
@@ -35,10 +35,10 @@ import java.lang.ref.SoftReference
 class V2RayVpnService : VpnService(), ServiceControl {
     companion object {
         private const val VPN_MTU = 1500
-        private const val PRIVATE_VLAN4_CLIENT = "10.10.10.1"
-        private const val PRIVATE_VLAN4_ROUTER = "10.10.10.2"
-        private const val PRIVATE_VLAN6_CLIENT = "fc00::10:10:10:1"
-        private const val PRIVATE_VLAN6_ROUTER = "fc00::10:10:10:2"
+        private const val PRIVATE_VLAN4_CLIENT = "10.10.14.1"
+        private const val PRIVATE_VLAN4_ROUTER = "10.10.14.2"
+        private const val PRIVATE_VLAN6_CLIENT = "fc00::10:10:14:1"
+        private const val PRIVATE_VLAN6_ROUTER = "fc00::10:10:14:2"
         private const val TUN2SOCKS = "libtun2socks.so"
     }
 
