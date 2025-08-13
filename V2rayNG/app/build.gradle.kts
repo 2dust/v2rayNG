bf01fe2bdb9ce8ce0323d153abb99d1cba4f52d6
2dust <31833384+2dust@users.noreply.github.com>
2025-08-13 08:48:33 +0800

    up 1.10.18

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 21
         targetSdk = 35
-        versionCode = 667
-        versionName = "1.10.17"
+        versionCode = 668
+        versionName = "1.10.18"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
