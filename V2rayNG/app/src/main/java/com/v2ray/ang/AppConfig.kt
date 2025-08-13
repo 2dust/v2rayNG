8a0e82749fb58133ed15e51b282eb4c4ee82012d
DHR60 <dehongren60@gmail.com>
2025-08-13 12:17:55 +0800

    rename

--- V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -91,8 +91,8 @@ object AppConfig {
     const val TAG_DIRECT = "direct"
     const val TAG_BLOCKED = "block"
     const val TAG_FRAGMENT = "fragment"
-    const val TAG_DNS = "dns_module"
-    const val TAG_DOMESTIC_DNS = "domestic_dns"
+    const val TAG_DNS = "dns-module"
+    const val TAG_DOMESTIC_DNS = "domestic-dns"
 
     /** Network-related constants. */
     const val UPLINK = "uplink"
