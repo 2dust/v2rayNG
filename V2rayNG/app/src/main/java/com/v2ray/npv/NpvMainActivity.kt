f1cb22dfcbe94b43ec32ea58fe576d25a074663b
WY <eagle@gmail.com>
2025-11-03 14:45:02 +0800

    update package

--- V2rayNG/app/src/main/java/com/v2ray/npv/NpvMainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/npv/NpvMainActivity.kt
@@ -31,7 +31,7 @@ import com.v2ray.ang.extension.toast
 import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MmkvManager
-import com.v2ray.ang.service.V2RayServiceManager
+import com.v2ray.ang.handler.V2RayServiceManager
 import com.v2ray.ang.ui.AboutActivity
 import com.v2ray.ang.ui.CheckUpdateActivity
 import com.v2ray.ang.ui.LogcatActivity
