d8983a47615e8a24f6f1254bd21b0b91f20535e9
2dust <31833384+2dust@users.noreply.github.com>
2026-01-20 21:07:51 +0800

    Bug fix
    https://github.com/2dust/v2rayNG/issues/5186

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/GroupPagerAdapter.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/GroupPagerAdapter.kt
@@ -11,6 +11,8 @@ import com.v2ray.ang.dto.GroupMapItem
 class GroupPagerAdapter(activity: FragmentActivity, var groups: List<GroupMapItem>) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int = groups.size
     override fun createFragment(position: Int) = GroupServerFragment.newInstance(groups[position].id)
+    override fun getItemId(position: Int): Long = groups[position].id.hashCode().toLong()
+    override fun containsItem(itemId: Long): Boolean = groups.any { it.id.hashCode().toLong() == itemId }
 
     @SuppressLint("NotifyDataSetChanged")
     fun update(groups: List<GroupMapItem>) {
