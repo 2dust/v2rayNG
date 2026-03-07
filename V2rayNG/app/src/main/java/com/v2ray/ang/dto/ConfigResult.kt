65e72a92ab7bff8aefe4368d67a1eab646298516
Zhang Licai <122581532+Zhang-Licai@users.noreply.github.com>
2026-03-07 10:09:44 +0800

    Delete V2rayNG directory

--- V2rayNG/app/src/main/java/com/v2ray/ang/dto/ConfigResult.kt
+++
@@ -1,8 +0,0 @@
-package com.v2ray.ang.dto
-
-data class ConfigResult(
-    var status: Boolean,
-    var guid: String? = null,
-    var content: String = "",
-)
-
--- V2rayNG/app/src/main/java/com/v2ray/ang/dto/WebDavConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.v2ray.ang.dto
-
-data class WebDavConfig(
-    val baseUrl: String,
-    val username: String? = null,
-    val password: String? = null,
-    val remoteBasePath: String = "/",
-    val timeoutSeconds: Long = 30
-)
