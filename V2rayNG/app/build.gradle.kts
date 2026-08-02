31d34a7944f1f34e945d7fd710accf833882a995
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2026-08-02 12:38:51 +0500

    app update

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.vanguard.client"
         minSdk = 24
         targetSdk = 37
         versionCode = 741
