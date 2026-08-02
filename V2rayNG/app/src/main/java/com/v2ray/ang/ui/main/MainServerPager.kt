28bb5c7df755f3e60197a9190c4a836e7884836b
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2026-08-02 18:46:58 +0500

    Import Color in MainServerPager.kt
    Added import for Color in MainServerPager.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainServerPager.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainServerPager.kt
@@ -36,6 +36,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineBreak
@@ -429,4 +430,3 @@ internal suspend fun PagerState.navigateToPageOptimized(
         scrollToPage(target)
     }
 }
-c
