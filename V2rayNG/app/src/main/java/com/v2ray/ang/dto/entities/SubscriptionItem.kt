759cefa00256a1c28f23aa8f8d7ca8de40f87892
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2026-08-03 16:49:43 +0500

    Update SubscriptionItem with new properties

--- V2rayNG/app/src/main/java/com/v2ray/ang/dto/entities/SubscriptionItem.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/dto/entities/SubscriptionItem.kt
@@ -6,8 +6,8 @@ data class SubscriptionItem(
     var enabled: Boolean = true,
     val addedTime: Long = System.currentTimeMillis(),
     var lastUpdated: Long = -1,
-    var autoUpdate: Boolean = false,
-    var updateInterval: Long = 1440, // in minutes, default to 24 hours
+    var autoUpdate: Boolean = true,
+    var updateInterval: Long = 1440,
     var prevProfile: String? = null,
     var nextProfile: String? = null,
     var filter: String? = null,
@@ -15,5 +15,9 @@ data class SubscriptionItem(
     var userAgent: String? = null,
     var requestHeaders: String? = null,
     var announce: String = "",
+    var supportUrl: String = "",
+    var trafficUpload: Long = 0L,
+    var trafficDownload: Long = 0L,
+    var trafficTotal: Long = 0L,
+    var trafficExpire: Long = 0L
 )
-
