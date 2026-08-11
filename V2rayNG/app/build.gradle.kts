4a1760fb8696a3b00fbd1edd6707b0ac495742cf
aliRahimi1997 <83342120+aliRahimi1997@users.noreply.github.com>
2026-08-11 08:30:23 +0330

    app pack name change

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.v2ray.ang2"
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.v2ray.ang2"
         minSdk = 24
         targetSdk = 37
         versionCode = 743
