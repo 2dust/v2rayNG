196725bbd37675bfa5e6f69e0b652a9ada6ef2a0
patterniha <71074308+patterniha@users.noreply.github.com>
2026-08-25 23:35:44 +0330

    Revert "Merge pull request #9 from mmrabbani/patch-10"
    This reverts commit b738a12f66cc46560ad931061d782295dc1a3c70, reversing
    changes made to ec9e53eed58a3f129b0abca4fc6e637b82d396c2.

--- V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/AppConfig.kt
@@ -147,7 +147,7 @@ object AppConfig {
 
     /** DNS server addresses. */
     const val DNS_PROXY = "https://cloudflare-dns.com/dns-query"
-    const val DNS_DIRECT = "8.8.8.8"
+    const val DNS_DIRECT = "223.5.5.5"
     const val DNS_VPN = "1.1.1.1"
     const val GEOSITE_PRIVATE = "geosite:private"
     const val GEOSITE_CN = "geosite:cn"
